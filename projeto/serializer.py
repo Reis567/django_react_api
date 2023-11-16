4caf2b8e3b6b4a8d29c4588716fbca53de416327
Reis567 <matheusdosreis9@gmail.com>
2023-11-16 19:33:28 -0300

    create job serializer

--- projeto/serializer.py
+++ projeto/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from projeto.models import Profissional
+from projeto.models import Profissional,Job
 
 
 
@@ -7,3 +7,9 @@ class ProfissionalSerializer(serializers.ModelSerializer ):
     class Meta :
         model = Profissional
         fields = '__all__'
+
+
+class CadastrarJobSerializer(serializers.ModelSerializer):
+    nome = serializers.CharField(max_lenght=100)
+    email = serializers.EmailField(max_lenght=255)
+
