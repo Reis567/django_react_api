5fd558e6322824fcb6499b9c2dc4614ac10107d3
Reis567 <matheusdosreis9@gmail.com>
2023-11-16 06:59:41 -0300

    API VIEW

--- projeto/views.py
+++ projeto/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.views import APIView , Response
+from projeto.models import Profissional
 
 # Create your views here.
+
+class ProfissinalAPI(APIView):
+    profissionais = Profissional.objects.all()
