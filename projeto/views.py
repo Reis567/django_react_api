f5f5b5cbaa7fbe916606f628a547d5442963598c
Reis567 <matheusdosreis9@gmail.com>
2023-11-16 18:53:17 -0300

    status return

--- projeto/views.py
+++ projeto/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from rest_framework.views import APIView , Response
 from projeto.models import Profissional
 from projeto.serializer import ProfissionalSerializer
+from rest_framework.status import HTTP_200_OK
+
+
 # Create your views here.
 
 class ProfissinalAPI(APIView):
     def get(self, request , format=None):
         profissionais = Profissional.objects.all()
         serializer = ProfissionalSerializer(profissionais, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=HTTP_200_OK)
