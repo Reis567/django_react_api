a04246a4689a5f3576bc14c2e2db9c8bbf27c1f1
Reis567 <matheusdosreis9@gmail.com>
2023-11-17 07:41:44 -0300

    logic to add on post view

--- projeto/views.py
+++ projeto/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from rest_framework.views import APIView , Response
-from projeto.models import Profissional
-from projeto.serializer import ProfissionalSerializer
+from projeto.models import Profissional,Job
+from projeto.serializer import ProfissionalSerializer,CadastrarJobSerializer
 from rest_framework.status import HTTP_200_OK
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -12,3 +13,13 @@ class ProfissinalAPI(APIView):
         profissionais = Profissional.objects.all()
         serializer = ProfissionalSerializer(profissionais, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
+class CadastrarJobAPI(APIView):
+    def post(self, request , id , format=None):
+        profissional = get_object_or_404(profissional , id=id)
+        serializer = CadastrarJobSerializer(data=request.data)
+        if serializer.is_valid():
+            job = Job(
+                nome = serializer.validated_data.get('nome'),
+                email = serializer.validated_data.get('email'),
+                profissional = profissional
+            )
