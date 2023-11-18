b3c2101944a9d5b2ab66eba33b46b45068247633
Reis567 <matheusdosreis9@gmail.com>
2023-11-18 18:27:27 -0300

    cors allow all origin

--- hireme/settings.py
+++ hireme/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,4 +132,4 @@ REST_FRAMEWORK = {
 }
 
 
-CORS_ALLOWS_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
